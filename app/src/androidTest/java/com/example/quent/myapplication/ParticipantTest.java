4a5eab0ac18be4180ee76d8b31aaff43b380dfbd
Quentin Berne <quentin.berne@gmail.com>
2016-10-04 17:08:32 +0200

    TP3

--- app/src/androidTest/java/com/example/quent/myapplication/ParticipantTest.java
+++ app/src/androidTest/java/com/example/quent/myapplication/ParticipantTest.java
@@ -10,36 +10,36 @@ import java.util.Date;
  */
 
 public class ParticipantTest extends TestCase {
+
+    Participant p;
+    SimpleDateFormat sdf;
+
     public ParticipantTest(String testMethodName) {
         super(testMethodName);
     }
 
-    public Participant testInstanciation() throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        return new Participant("de Robien", "Frédérique", sdf.parse("01/01/2001"), (byte) 49);
+    protected void setUp() throws Exception {
+        super.setUp();
+        sdf = new SimpleDateFormat("dd/MM/yyyy");
+        p = new Participant("de Robien", "Frédérique", sdf.parse("01/01/2001"), (byte) 49);
     }
 
     public void testNom() throws Exception {
-        Participant p = testInstanciation();
         String n = p.getNom();
         assertEquals("Erreur dans les accesseurs de nom", n, "de Robien");
     }
 
     public void testPrenom() throws Exception {
-        Participant p = testInstanciation();
         String n = p.getPrenom();
         assertEquals("Erreur dans les accesseurs de prenom", n, "Frédérique");
     }
 
     public void testDateDeNaissance() throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Participant p = testInstanciation();
         Date d = p.getDateDeNaissance();
         assertEquals("Erreur dans les accesseurs de dateDeNaissance", d, sdf.parse("01/01/2001"));
     }
 
     public void testNumDepartement() throws Exception {
-        Participant p = testInstanciation();
         byte b = p.getNumDepartement();
         assertEquals(49, b);
         p.setNumDepartement((byte) 55);
@@ -48,8 +48,6 @@ public class ParticipantTest extends TestCase {
     }
 
     public void testToString() throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Participant p = testInstanciation();
         String n = p.toString();
         assertEquals("Erreur dans la methode toString()", n, "Participant\nde Robien\nFrédérique\n" + sdf.parse("01/01/2001") + "\n49");
     }
